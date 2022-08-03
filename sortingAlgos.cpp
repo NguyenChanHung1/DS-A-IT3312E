bec0b4ca36e7aaba06581dce99b4127a3133099b
NguyenChanHung1 <100984506+NguyenChanHung1@users.noreply.github.com>
2022-08-03 10:15:52 +0700

    Update sortingAlgos.cpp

--- sortingAlgos.cpp
+++ sortingAlgos.cpp
@@ -136,7 +136,49 @@ position */
     }
 } // Time complexity O(nlogn)
 
-void HeapSort(int a[],int low,int high);
+void Cswap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void Heapify(int arr[],int N,int i){
+    // Find largest among root, left child and right child
+    // Initialize largest as root
+    int largest = i;
+    // left = 2*i + 1
+    int left = 2 * i + 1;
+    // right = 2*i + 2
+    int right = 2 * i + 2;
+    // If left child is larger than root
+    if (left < N && arr[left] > arr[largest])
+        largest = left;
+    // If right child is larger than largest
+    // so far
+    if (right < N && arr[right] > arr[largest])
+        largest = right;
+    // Swap and continue heapifying if root is not largest
+    // If largest is not root
+    if (largest != i) {
+        Cswap(&arr[i], &arr[largest]);
+        // Recursively heapify the affected
+        // sub-tree
+        Heapify(arr, N, largest);
+    }
+}
+
+void HeapSort(int arr[],int N){
+    // Build max heap
+    for(int i=N/2-1;i>=0;--i){
+        Heapify(arr,N,i);
+    }
+    // Heap sort
+    for(int i=N-1;i>=0;i--){
+        Cswap(&arr[0],&arr[i]);
+        // Heapify root element to get the highest element at root again
+        Heapify(arr,i,0);
+    }
+}
 
 int main(){
     int n;
